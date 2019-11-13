43f39f28769162249ed443343f3325b22636d432
Jiaan Dai <jiaand@andrew.cmu.edu>
2019-11-13 03:08:22 +0000

    Update makefile and interface

--- include/queue.h
+++ include/queue.h
@@ -1,6 +1,8 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
+#include <optional>
+
 template <typename T>
 class Queue {
  public:
@@ -8,7 +10,7 @@ class Queue {
   virtual ~Queue() = default;
 
   virtual void Push(T&& value) = 0;
-  virtual T&& Pop() = 0;
+  virtual std::optional<T> Pop() = 0;
 };
 
 #endif  // _QUEUE_H_
