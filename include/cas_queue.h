df7ce5692108c7a81edaaae35669271c70aee8d5
Yunjia Wang <wangyunjia1996@gmail.com>
2019-11-16 05:29:48 -0500

    Simple benchmark

--- include/cas_queue.h
+++ include/cas_queue.h
@@ -11,7 +11,7 @@ template <typename T>
 class CasQueue : public Queue<T> {
  public:
   CasQueue() : head_(nullptr), tail_(nullptr) {
-    fprintf(stderr, "%s\n", "\n Initialize \n");
+    // fprintf(stderr, "%s\n", "\n Initialize \n");
   }
 
   virtual void Push(const T& value) {
@@ -20,22 +20,26 @@ class CasQueue : public Queue<T> {
     Node* curr_tail;
     while (1) {
       curr_tail = tail_.load(std::memory_order_acquire);
+      // fprintf(stderr, "push, curr_tail = %d\n", curr_tail);
       if (tail_.compare_exchange_weak(curr_tail, new_tail,
                                       std::memory_order_release,
                                       std::memory_order_relaxed)) {
-        if (curr_tail == nullptr) {
-          fprintf(stderr, "set head\n");
-          head_.store(new_tail, std::memory_order_release);
-        } else {
-          curr_tail->next_ = new_tail;
-        }
+        // fprintf(stderr, "Pushed %d\n",value);
         Node* nullp = nullptr;
         head_.compare_exchange_strong(nullp, new_tail,
                                       std::memory_order_release,
                                       std::memory_order_relaxed);
-        fprintf(stderr, "curr_tail = %d, Push %d at %d, Queue size %d \n", curr_tail, value, new_tail, count_.load(std::memory_order_acquire));
+        if (curr_tail != nullptr) {
+          // fprintf(stderr, "push, set head to %d (%d)\n", new_tail, value);
+          // head_.store(new_tail, std::memory_order_release);
+        // } else {
+          curr_tail->next_ = new_tail;
+        }
+        
+        
         // atomic_fetch_add_explicit(&count_, 1, std::memory_order_release);
         count_.fetch_add(1, std::memory_order_release);
+        // fprintf(stderr, "push, curr_tail = %d, Push %d at %d, Queue size %d \n", curr_tail, value, new_tail, count_.load(std::memory_order_acquire));
         break;
       }
       
@@ -47,32 +51,42 @@ class CasQueue : public Queue<T> {
 
   virtual std::optional<T> Pop() {
     Node* curr_head;
+    Node* curr_tail;
     std::optional<T> optval;
+    struct timespec tim, tim2;
+    tim.tv_sec = 0;
+    tim.tv_nsec = 500;
 
     while (1) {
       
-      if (count_.load(std::memory_order_acquire) == 0) {
+      if (count_.load(std::memory_order_acquire) <= 0) {
         // fprintf(stderr, "%s\n", "Returned");
         return optval;
       }
-      fprintf(stderr, "Pop, Queue size %d \n", count_.load(std::memory_order_acquire));
+      // fprintf(stderr, "Pop, Queue size %d \n", count_.load(std::memory_order_acquire));
       curr_head = head_.load(std::memory_order_acquire);
-      fprintf(stderr, "curr_head = %d, value = %d\n", curr_head, curr_head->value_);
+      curr_tail = tail_.load(std::memory_order_acquire);
+      // fprintf(stderr, "curr_head = %d\n", curr_head);
+      if (curr_head == nullptr) return optval;
+      // fprintf(stderr, "pop, curr_head = %d, value = %d, next = %d, tail = %d\n", curr_head, curr_head->value_, curr_head->next_, tail_.load(std::memory_order_acquire));
+      if (curr_head->next_ == nullptr && curr_tail != nullptr && curr_tail != curr_head) return optval;
       if (head_.compare_exchange_weak(curr_head, curr_head->next_,
                                       std::memory_order_release,
                                       std::memory_order_relaxed)) {
         // fprintf(stderr, "1 - curr_head = %d\n", curr_head);
         auto head_check = curr_head;
-        tail_.compare_exchange_strong(head_check, nullptr,
+        tail_.compare_exchange_strong(head_check, curr_head->next_,
                                     std::memory_order_release,
                                     std::memory_order_relaxed);
         // fprintf(stderr, "2 - curr_head = %d\n", curr_head);
         optval = move(curr_head->value_);
         // fprintf(stderr, "deleting %d\n", curr_head);
         delete curr_head;
-        fprintf(stderr, "new head = %d\n", head_.load(std::memory_order_acquire));
+        // curr_head = nullptr;
+        
         // atomic_fetch_sub_explicit(&count_, 1, memory_order_release);
         count_.fetch_sub(1, std::memory_order_release);
+        // fprintf(stderr, "pop, count down to %d new head = %d tail %d \n", count_.load(std::memory_order_relaxed), head_.load(std::memory_order_acquire), tail_.load(std::memory_order_acquire));
         return optval;
       }
     }
--- src/main.cc
+++ src/main.cc
@@ -2,35 +2,79 @@
 #include <string.h>
 #include <unistd.h>
 
-using std::string;
+#include "cas_queue.h"
 
-template <typename... Args>
-string StrFormat(const char* format, Args... args) {
-  int size = snprintf(NULL, 0, format, args...);
-  string buf(size + 1, '\0');
-  snprintf(&buf[0], buf.size(), format, args...);
-  return buf;
-}
+using namespace std;
 
-static constexpr const char* key = "e";
-static constexpr double value = 2.718281828459;
-static constexpr const char* format = "%s: %.6f\n";
+// template <typename... Args>
+// string StrFormat(const char* format, Args... args) {
+//   int size = snprintf(NULL, 0, format, args...);
+//   string buf(size + 1, '\0');
+//   snprintf(&buf[0], buf.size(), format, args...);
+//   return buf;
+// }
 
-static void StrFormatBenchmark(benchmark::State& state) {
+// static constexpr const char* key = "e";
+// static constexpr double value = 2.718281828459;
+// static constexpr const char* format = "%s: %.6f\n";
+
+// static void StrFormatBenchmark(benchmark::State& state) {
+//   for (auto _ : state) {
+//     string s = StrFormat(format, key, value);
+//     assert(s.size() > 0);
+//     usleep(10);  // 10 us
+//   }
+// }
+// BENCHMARK(StrFormatBenchmark);
+
+// static void SnprintfBenchmark(benchmark::State& state) {
+//   for (auto _ : state) {
+//     char buf[4096];
+//     int ret = snprintf(buf, 4096, format, key, value);
+//     assert(ret > 0);
+//     usleep(10);  // 10 us
+//   }
+// }
+// BENCHMARK(SnprintfBenchmark);
+
+static void CASQueuePushBenchmark(benchmark::State& state) {
+  static CasQueue<int>* casq = nullptr;
+  // Setup for each run of test.
+  if (state.thread_index == 0) {
+    casq = new CasQueue<int>();
+  }
+  
   for (auto _ : state) {
-    string s = StrFormat(format, key, value);
-    assert(s.size() > 0);
-    usleep(10);  // 10 us
+    casq->Push(0);
+  }
+
+  // Teardown at the end of each test run.
+  if (state.thread_index == 0) {
+    delete casq;
   }
 }
-BENCHMARK(StrFormatBenchmark);
+BENCHMARK(CASQueuePushBenchmark)->ThreadRange(1, 256);
 
-static void SnprintfBenchmark(benchmark::State& state) {
-  for (auto _ : state) {
-    char buf[4096];
-    int ret = snprintf(buf, 4096, format, key, value);
-    assert(ret > 0);
-    usleep(10);  // 10 us
+static void CASQueuePopBenchmark(benchmark::State& state) {
+  static CasQueue<int>* casq = nullptr;
+  
+  if (state.thread_index == 0) {
+    const int num_items = state.range(0);
+    casq = new CasQueue<int>();
+    for (int i = 0; i < num_items; i++) {
+      casq->Push(0);
+    }
+  }
+
+  const int num_iterations = state.range(0) / state.threads;
+  while (state.KeepRunningBatch(num_iterations)) {
+    auto ret = casq->Pop();
+  }
+
+  // Teardown at the end of each test run.
+  if (state.thread_index == 0) {
+    delete casq;
   }
 }
-BENCHMARK(SnprintfBenchmark);
+
+BENCHMARK(CASQueuePushBenchmark)->ThreadRange(1, 256)->Arg(256000);
