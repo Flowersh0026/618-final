558dc4fc0d4e8920603a189f3a79750dcb937ceb
Jiaan Dai <jiaand@andrew.cmu.edu>
2019-12-03 23:10:06 -0500

    Clean up comments and reformat code

--- include/lock_queue.h
+++ include/lock_queue.h
@@ -2,8 +2,9 @@
 #define _LOCK_QUEUE_H_
 
 #include "queue.h"
-#include <mutex>
+
 #include <atomic>
+#include <mutex>
 
 using namespace std;
 
@@ -16,41 +17,39 @@ class LockQueue : public Queue<T> {
 
   virtual void Push(const T& value) {
     lock_guard<mutex> lock(mu_);
-    // fprintf(stderr, "Push\n");
     if (tail_ == nullptr) {
       tail_ = head_ = new Node(value);
     } else {
       tail_->next_ = new Node(value);
       tail_ = tail_->next_;
     }
-    count_.store(count_.load(std::memory_order_relaxed) + 1, std::memory_order_relaxed);
+    count_.store(count_.load(std::memory_order_relaxed) + 1,
+                 std::memory_order_relaxed);
   }
 
   virtual void Push(T&& value) {
     lock_guard<mutex> lock(mu_);
-    // fprintf(stderr, "Push 2\n");
     if (tail_ == nullptr) {
       tail_ = head_ = new Node(value);
     } else {
       tail_->next_ = new Node(value);
       tail_ = tail_->next_;
     }
-    count_.store(count_.load(std::memory_order_relaxed) + 1, std::memory_order_relaxed);
+    count_.store(count_.load(std::memory_order_relaxed) + 1,
+                 std::memory_order_relaxed);
   }
 
   virtual optional<T> Pop() {
     lock_guard<mutex> lock(mu_);
-    // fprintf(stderr, "Pop\n");
     std::optional<T> optval;
-    // fprintf(stderr, "count = %d\n", count_.load(std::memory_order_relaxed));
     if (count_.load(std::memory_order_relaxed) == 0) return optval;
     optval = move(head_->value_);
     auto old_head = head_;
     head_ = head_->next_;
     delete old_head;
     if (head_ == nullptr) tail_ = nullptr;
-    count_.store(count_.load(std::memory_order_relaxed) - 1, std::memory_order_relaxed);
-    // fprintf(stderr, "Pop Fin\n");
+    count_.store(count_.load(std::memory_order_relaxed) - 1,
+                 std::memory_order_relaxed);
     return optval;
   }
 
@@ -68,10 +67,6 @@ class LockQueue : public Queue<T> {
 
   mutex mu_;
   atomic<int> count_;
-
-
-
-
 };
 
 #endif  // _LOCK_QUEUE_H_
